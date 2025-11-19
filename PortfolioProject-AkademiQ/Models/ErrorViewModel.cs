04c4d09badbe9ad5039082e2ac81529ac00b305b
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-11-19 20:20:15 +0300

    Add project files.

---
+++ PortfolioProject-AkademiQ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioProject_AkademiQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
