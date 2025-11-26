018cca41971bc89e99f971fd0dcbcc2af2958b4a
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-11-26 22:42:59 +0300

    Updated

---
+++ PortfolioProject-AkademiQ/ViewComponents/_DefaultAboutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject_AkademiQ.ViewComponents
+{
+    public class _DefaultAboutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioProject-AkademiQ/ViewComponents/_DefaultFeatureComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject_AkademiQ.ViewComponents
+{
+    public class _DefaultFeatureComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+        }
+}
