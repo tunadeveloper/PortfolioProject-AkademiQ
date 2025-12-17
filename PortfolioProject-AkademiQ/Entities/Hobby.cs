4347c3e9e731034481b3a66bc7a52fec17cb0115
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-12-17 15:26:30 +0300

    Updated

---
+++ PortfolioProject-AkademiQ/Entities/Hobby.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PortfolioProject_AkademiQ.Entities;
+
+public partial class Hobby
+{
+    public int? HobbyId { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? IconUrl { get; set; }
+}
