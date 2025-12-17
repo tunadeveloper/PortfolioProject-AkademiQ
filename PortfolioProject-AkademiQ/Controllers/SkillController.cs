b7f7e5a44c54d62cfe577cda314ecca8e4b34d74
Tunahan Cengiz <140824629+tunadeveloper@users.noreply.github.com>
2025-12-17 21:57:13 +0300

    updated

---
+++ PortfolioProject-AkademiQ/Controllers/SkillController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioProject_AkademiQ.Data;
+
+namespace PortfolioProject_AkademiQ.Controllers
+{
+    public class SkillController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SkillController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _context.Skills.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ PortfolioProject-AkademiQ/ViewComponents/AdminLayoutViewComponents/_AdminLayoutSideBarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject_AkademiQ.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutSideBarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
