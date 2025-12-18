364421a8e23f402844dd5993deb1383af9175f41
Tunahan Cengiz <tunahancengizyt250@gmail.com>
2025-12-18 21:40:47 +0300

    Updated

--- PortfolioProject-AkademiQ/Controllers/SkillController.cs
+++ PortfolioProject-AkademiQ/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PortfolioProject_AkademiQ.Data;
+using PortfolioProject_AkademiQ.Entities;
 
 namespace PortfolioProject_AkademiQ.Controllers
 {
@@ -16,6 +17,39 @@ namespace PortfolioProject_AkademiQ.Controllers
         {
             var values = _context.Skills.ToList();
             return View(values);
+        }
+        public IActionResult CreateSkill() => View();
+
+        [HttpPost]
+        public IActionResult CreateSkill(Skill skill)
+        {
+            _context.Skills.Add(skill);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = _context.Skills.Find(id);
+            _context.Skills.Remove(values);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UpdateSkill(int id)
+        {
+            var values = _context.Skills.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateSkill(Skill skill)
+        {
+            var values = _context.Skills.Find(skill.SkillId);
+            values.SkillTitle = skill.SkillTitle;
+            values.SkillValue = skill.SkillValue;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
